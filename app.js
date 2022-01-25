a0f701312bb7048c551e320b01ca23511dbec77a
Philip Tronser <tronser@web.de>
2022-01-25 19:55:26 +0100

    DOM basics

---
+++ app.js
@@ -0,0 +1,6 @@
+const header = document.getElementById('header');
+header.innerHTML = "Magic";
+header.style.color = 'red';
+header.style.border = '1px dotted blue';
+header.remove();
+console.log(header);
